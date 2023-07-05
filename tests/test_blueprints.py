e2810a1820d88b9f9198a53d6d7b79f68cafb047
Collin Heist <collinheist@gmail.com>
2023-07-05 00:34:30 -0600

    Create tests
    - Create pytests to validate any submitted Blueprints

---
+++ tests/test_blueprints.py
@@ -0,0 +1,95 @@
+from json import load as json_load, JSONDecodeError
+from pathlib import Path
+
+from re import compile as re_compile
+
+BLUEPRINT_FOLDER = Path('./blueprints/')
+
+# Non-tests
+
+def read_blueprints(skip_null: bool = True):
+    for blueprint_file in BLUEPRINT_FOLDER.glob('*/*/blueprints.json'):
+        series_subfolder = blueprint_file.parent
+        with blueprint_file.open('r') as file_handle:
+            for blueprint_id, blueprint in enumerate(json_load(file_handle)):
+                if skip_null and blueprint is None:
+                    continue
+
+                yield series_subfolder, blueprint_id, blueprint
+
+    return None
+
+# Tests
+
+class TestFolderOrganization:
+    def test_subfolder_names(self):
+        for folder in BLUEPRINT_FOLDER.glob('*'):
+            assert folder.name.upper() == folder.name, 'Folder Names Must Be Uppercase'
+
+    def test_series_folder_names(self):
+        NAME_REGEX = re_compile(r'^.+\(\d{4}\)$')
+        for folder in BLUEPRINT_FOLDER.glob('*/*'):
+            assert NAME_REGEX.match(folder.name), 'Series Folder Names Must be Formatted as "Name (Year)"'
+
+    def test_series_blueprint_folder_names(self):
+        for folder in BLUEPRINT_FOLDER.glob('*/*/*'):
+            if folder.is_file():
+                continue
+
+            assert folder.name.isdigit(), 'Series Blueprint subfolders must be named their blueprint ID'
+
+    def test_series_subfolder_files(self):
+        for file in BLUEPRINT_FOLDER.glob('*/*/*'):
+            if file.is_dir():
+                continue
+
+            assert file.name == 'blueprints.json', 'Only "blueprints.json" is allowed at root of Series Subfolder'
+
+
+class TestBlueprintJSON:
+    def test_blueprint_valid_json(self):
+        for file in BLUEPRINT_FOLDER.glob('*/*/blueprints.json'):
+            content = None
+            with file.open('r') as file_handle:
+                try:
+                    content = json_load(file_handle)
+                except JSONDecodeError:
+                    pass
+
+            assert content is not None, 'All blueprints.json files must be valid JSON and cannot be empty'
+            assert isinstance(content, list), 'All blueprint files must have a top-level list'
+
+    def test_blueprint_json_has_required_data(self):
+        for _, _, blueprint in read_blueprints():
+            assert isinstance(blueprint, dict), 'Invalid blueprint JSON'
+            assert 'description' in blueprint.keys(), 'All blueprints must have a "description" field'
+            assert isinstance(blueprint['description'], list) and len(blueprint['description']) > 0, 'All blueprint descriptions must be a list'
+            assert 'preview' in blueprint.keys(), 'All blueprints must have a "preview" field'
+            assert 'creator' in blueprint.keys(), 'All blueprints must have a "creator" field'
+
+
+class TestBlueprintSubfolders:
+    def test_blueprint_has_subfolder(self):
+        for series_subfolder, blueprint_id, _ in read_blueprints():
+            assert (series_subfolder / str(blueprint_id)).exists(), 'Every blueprint must have a subfolder'
+
+    def test_deleted_blueprints_has_no_subfolder(self):
+        for series_subfolder, blueprint_id, blueprint in read_blueprints(skip_null=False):
+            if blueprint is not None:
+                continue
+
+            assert not (series_subfolder / str(blueprint_id)).exists(), 'Deleted blueprints must have no subfolder'
+
+class TestBlueprintFiles:
+    def test_blueprint_identifies_all_files(self):
+        for series_subfolder, blueprint_id, blueprint in read_blueprints():
+            # Get list of files in the blueprint subfolder
+            given_files = {file.name for file in (series_subfolder / str(blueprint_id)).glob('*')}
+            # Get all files listed in any Fonts
+            font_files = {
+                font['file']
+                for font in blueprint.get('fonts', [])
+                if font.get('file', None) is not None
+            }
+            assert len(given_files - font_files - {blueprint['preview']}) == 0, 'Only files listed in the blueprint can be included in the blueprint folder'
+            assert len((font_files | {blueprint['preview']}) - given_files) == 0, 'All files listed in the blueprint must be included in the blueprint folder'
