f28d7c02c5b573ee4868bdfa90211ee3c4be07fe
Collin Heist <collinheist@gmail.com>
2023-07-05 23:57:20 -0600

    Update helper to create path safe folder names

--- helper.py
+++ helper.py
@@ -1,7 +1,42 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
+from re import sub as re_sub, IGNORECASE
+
 BLUEPRINT_FOLDER = Path(__file__).parent / 'blueprints'
+
+
+def get_path_name(series_name: str) -> str:
+    """
+    Get the path-safe name for the given Series name.
+
+    Args:
+        series_name: Name of the Series.
+
+    Returns:
+        Path-safe name with prefix a/an/the and any illegal characters
+        (e.g. '?', '|', '/', etc.) removed.
+    """
+
+    return re_sub(
+        r'^(a|an|the)(\s)',
+        '',
+        str(series_name).translate(
+            str.maketrans({
+                '?': '!',
+                '<': '',
+                '>': '',
+                ':':' -',
+                '"': '',
+                '|': '',
+                '*': '-',
+                '/': '+',
+                '\\': '+',
+            })
+        ),
+        flags=IGNORECASE
+    )
+
 
 # File is entrypoint
 if __name__ == '__main__':
@@ -11,11 +46,12 @@ if __name__ == '__main__':
     args = ap.parse_args()
 
     # Create all subfolder
-    folder = BLUEPRINT_FOLDER / str(args.series[0].upper()) / str(args.series)
+    series_name = get_path_name(args.series)
+    folder = BLUEPRINT_FOLDER / series_name[0].upper() / series_name
     folder.mkdir(exist_ok=True, parents=True)
 
     # Log
-    print(f'Created "blueprints/{args.series[0].upper()}/{args.series}"')
+    print(f'Created "blueprints/{series_name[0].upper()}/{series_name}"')
 
     # Create blueprints.json if DNE
     file = folder / 'blueprints.json'
