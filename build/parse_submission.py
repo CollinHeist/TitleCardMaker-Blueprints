489b0705524617e87de2e28c57791473001eda29
Collin Heist <collinheist@gmail.com>
2023-07-17 15:27:28 -0600

    Separate preview and font files in issue template/parser

--- build/parse_submission.py
+++ build/parse_submission.py
@@ -17,6 +17,7 @@ from requests import get
 
 ROOT = Path(__file__).parent.parent
 BLUEPRINT_FOLDER = ROOT / 'blueprints'
+TEMP_PREVIEW_FILE = ROOT / 'preview.jpg'
 TEMP_FILE = ROOT / 'tmp'
 TEMP_DIRECTORY = ROOT / 'unzipped'
 PATH_SAFE_TRANSLATION = str.maketrans({
@@ -56,12 +57,13 @@ if __name__ == '__main__':
     try:
         content = loads(environ.get('ISSUE_BODY'))
         print(f'Parsed issue JSON as:\n{content}')
-    except JSONDecodeError:
+    except JSONDecodeError as exc:
         print(f'Unable to parse Context as JSON')
+        print(exc)
         sys_exit(1)
 
     # Get the issue's author and the body (the issue text itself)
-    creator = environ.get('ISSUE_CREATOR')
+    creator = environ.get('ISSUE_CREATOR', 'CollinHeist')
 
     # Extract the data from the issue text
     issue_regex = re_compile(
@@ -69,8 +71,9 @@ if __name__ == '__main__':
         r'### Series Year\s+(?P<series_year>\d+)\s+'
         r'### Creator Username\s+(?P<creator>.+)\s+'
         r'### Blueprint Description\s+(?P<description>[\s\S]*)\s+'
-        r'### Blueprint File\s+```json\s+(?P<blueprint>[\s\S]*?)```\s+'
-        r'### Zip of Files\s+.*?\[.*\]\((?P<file_url>.+)\).*$'
+        r'### Blueprint\s+```json\s+(?P<blueprint>[\s\S]*?)```\s+'
+        r'### Preview Title Card\s+.*?\[.*\]\((?P<preview_url>.+)\)\s+'
+        r'### Zip of Font Files\s+(?P<font_zip>_No response_|\[.*\]\((.+)\))$'
     )
 
     # If data cannot be extracted, exit
@@ -81,13 +84,15 @@ if __name__ == '__main__':
 
     # Get each variable from the issue
     data = data_match.groupdict()
-    print(f'Regex data extracted: {data=!r}')
+    print(f'{data=}')
     series_name = data['series_name'].strip()
     series_year = data['series_year']
-    creator = (creator if 'No response_' in data['creator'] else data['creator']).strip()
+    creator = (creator if '_No response_' in data['creator'] else data['creator']).strip()
     description = data['description']
     blueprint = data['blueprint']
-    file_url = data['file_url']
+    preview_url = data['preview_url']
+    font_zip_url = None if '_No response_' in data['font_zip'] else data['font_zip']
+    print(f'Raw parsed data: {series_name=}\n[{series_year=}\n{creator=}\n{description=}\n{blueprint=}\n{preview_url=}\n{font_zip_url=}')
 
     # Parse blueprint as JSON
     try:
@@ -119,31 +124,39 @@ if __name__ == '__main__':
         'description': [line.strip() for line in description.splitlines() if line.strip()],
     }
 
-    # Download files
-    response = get(file_url, timeout=30)
-    if not response.ok:
-        print(f'Unable to download indicated files from "{file_url}"')
+    # Download preview
+    if not (response := get(preview_url, timeout=30)).ok:
+        print(f'Unable to download preview file from "{preview_url}"')
         print(response.content)
         sys_exit(1)
-    print(f'Downloaded "{file_url}"')
-    file_content = response.content
+    print(f'Downloaded preview "{preview_url}"')
 
-    # Write content to file
-    uploaded_filename = file_url.rsplit('/', maxsplit=1)[-1]
-    downloaded_file = ROOT / uploaded_filename
-    downloaded_file.write_bytes(file_content)
+    # Copy preview into blueprint folder
+    TEMP_PREVIEW_FILE.write_bytes(response.content)
+    copy_file(TEMP_PREVIEW_FILE, blueprint_subfolder / 'preview.jpg')
+    print(f'Copied "{preview_url}" into blueprints/{letter}/{folder_name}/{id_}/preview.jpg')
 
-    # Just an image
-    if uploaded_filename.lower().endswith(('.jpg', '.png', '.jpeg', '.tiff', '.webp', '.gif')):
-        # Copy image into blueprint folder
-        copy_file(downloaded_file, blueprint_subfolder / uploaded_filename)
-        print(f'Copied "{file_url}" into blueprints/{letter}/{folder_name}/{id_}/{uploaded_filename}')
-    # Zip of files
-    else:
+    # Add preview image to blueprint
+    finalized_blueprint['preview'] = 'preview.jpg'
+
+    # Download any font zip files if provided
+    if font_zip_url is not None:
+        if not (response := get(font_zip_url, timeout=30)).ok:
+            print(f'Unable to download zip from "{font_zip_url}"')
+            print(response.content)
+            sys_exit(1)
+        print(f'Downloaded "{font_zip_url}"')
+        zip_content = response.content
+        
+        # Write zip to file
+        uploaded_filename = font_zip_url.rsplit('/', maxsplit=1)[-1]
+        downloaded_file = ROOT / uploaded_filename
+        downloaded_file.write_bytes(zip_content)
+
         try:
             unpack_archive(downloaded_file, TEMP_DIRECTORY)
         except (ValueError, ReadError):
-            print(f'Unable to unzip provided files from "{file_url}"')
+            print(f'Unable to unzip provided files from "{font_zip_url}"')
             sys_exit(1)
 
         print(f'Unzipped {[file.name for file in TEMP_DIRECTORY.glob("*")]}')
